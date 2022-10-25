564f21a10362e9a55b6e73801bcacff9600dfbf5
jimmiandreansa <jimmiandreansa11@gmail.com>
2022-10-25 18:49:30 +0700

    initial commit

---
+++ backend/config/connectDB.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI)
+
+        console.log(`MongoDB Connected!`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
+
+// USE THIS FUNCTION IN server.js TO CONNECT TO mongoDB AND START THE SERVER!
+
+// const startServer = async () => {
+//     try {
+//         await connectDB()
+//         app.listen(PORT, () => {
+//             console.log(`Server runnning on port: ${PORT}`)
+//         })
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// startServer()
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const connectDB = require("./config/connectDB")
+const mongoose = require("mongoose")
+const Task = require("./models/taskModel")
+const taskRoutes = require("./routes/taskRoute")
+const cors = require("cors")
+
+const app = express()
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors({
+    origin: ["http://localhost:3000", "https://mern-task-app.onrender.com"]
+}))
+
+app.use("/api/tasks", taskRoutes)
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Home Page")
+})
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server runnning on port: ${PORT}`)
+        })
+    })
+    .catch((err) => console.log(err))
